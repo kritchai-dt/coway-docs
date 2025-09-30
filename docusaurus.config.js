1c999aae7c15337f01c3e452df4dc4dee60f54b9
Kritchai KC <kc.wichiansanga@COWAY390.local>
2025-09-30 15:05:02 +0700

    update auto-schedule docs content and images

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -50,24 +50,9 @@ const config = {
           sidebarPath: './sidebars.js',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ['rss', 'atom'],
-            xslt: true,
-          },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-          // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
-        },
+          editUrl: undefined,
+    },
+        blog: false,
         theme: {
           customCss: './src/css/custom.css',
         },
@@ -93,7 +78,6 @@ const config = {
             position: 'left',
             label: 'Start Here',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
       {
         href: 'https://coway.co.th',
         label: 'เว็บไซต์หลัก',
@@ -130,8 +114,8 @@ const config = {
             title: 'More',
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
+                label: 'ติดต่อเรา',
+                href: 'https://www.coway.co.th/th/consult-form',
               },
             ],
           },
--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,7 +5,7 @@ import styles from './styles.module.css';
 const FeatureList = [
   {
     title: 'ใช้งานง่าย ใคร ๆ ก็เข้าใจได้',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    img: '/img/people-pp01.png',
     description: (
       <>
         ไม่ว่าคุณจะเป็นพนักงานใหม่ ตัวแทนขาย ทีมบริการ หรือแม้แต่ลูกค้า Coway Docs 
@@ -16,7 +16,7 @@ const FeatureList = [
   },
   {
     title: 'รวมทุกข้อมูลไว้ในที่เดียว',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    img: '/img/document-pp02.png',
     description: (
       <>
         เรารวมข้อมูลสำคัญที่คุณต้องใช้ไว้ทั้งหมด ทั้งคู่มือการใช้งาน ระบบการทำงานของทีมต่าง ๆ 
@@ -26,7 +26,7 @@ const FeatureList = [
   },
   {
     title: 'พร้อมช่วยเหลือคุณทุกเมื่อ',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    img: '/img/service-pp01.png',
     description: (
       <>
         Coway Docs พร้อมเป็นผู้ช่วยของคุณตลอดเวลา จะเข้าจากมือถือหรือคอมก็ได้ 
@@ -36,14 +36,18 @@ const FeatureList = [
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({Svg, img, title, description}) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+        {Svg ? (
+          <Svg className={styles.featureSvg} role="img" />
+        ) : (
+          <img src={img} className={styles.featureImg} alt={title} />
+        )}
       </div>
       <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
+        <h3>{title}</h3>
         <p>{description}</p>
       </div>
     </div>
