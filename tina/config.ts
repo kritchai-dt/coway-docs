5575a6b45bd379012a2f1e650bebe43ab8da1578
kc.kritchai <kritchaiw@coway.co.th>
2025-10-06 15:17:17 +0700

    fix token

--- tina/config.ts
+++ tina/config.ts
@@ -1,24 +1,19 @@
-// tina/config.ts
 import { defineConfig } from "tinacms";
 
 export default defineConfig({
-  branch: "main",
-  clientId: "3f96a547-a2f5-405a-a19c-fe76f1256275",
-  token: "07f0dd3e3f3993944167705e06ec17099f66646f",
-
-
+  branch: process.env.TINA_BRANCH || "main",
+  clientId: process.env.TINA_PUBLIC_CLIENT_ID!,
+  token: process.env.TINA_TOKEN!,
   build: {
-    outputFolder: "static/admin",
-    publicFolder: "static"
+    outputFolder: "admin",
+    publicFolder: "static",
   },
-
   media: {
     tina: {
       publicFolder: "static",
-      mediaRoot: "img/uploads"
-    }
+      mediaRoot: "img/uploads",
+    },
   },
-
   schema: {
     collections: [
       {
@@ -28,30 +23,9 @@ export default defineConfig({
         format: "md",
         fields: [
           { type: "string", name: "title", label: "หัวข้อ" },
-          { type: "number", name: "sidebar_position", label: "ลำดับใน Sidebar", required: false },
-          { type: "rich-text", name: "body", label: "เนื้อหา", isBody: true }
-        ]
+          { type: "rich-text", name: "body", label: "เนื้อหา", isBody: true },
+        ],
       },
-      {
-        label: "Service",
-        name: "service",
-        path: "docs/service",
-        format: "md",
-        fields: [
-          { type: "string", name: "title", label: "หัวข้อ" },
-          { type: "rich-text", name: "body", label: "เนื้อหา", isBody: true }
-        ]
-      },
-      {
-        label: "Account",
-        name: "account",
-        path: "docs/account",
-        format: "md",
-        fields: [
-          { type: "string", name: "title", label: "หัวข้อ" },
-          { type: "rich-text", name: "body", label: "เนื้อหา", isBody: true }
-        ]
-      }
-    ]
-  }
+    ],
+  },
 });
