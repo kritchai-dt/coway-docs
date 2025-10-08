748af36e74c17eb36c64e882b3eb3b9cd9d4450d
kc.kritchai <kritchaiw@coway.co.th>
2025-10-08 08:30:02 +0700

    chore: remove TinaCMS completely

--- tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '812fc5570381ec86d161bf58e302963bc8a5a809', queries,  });
-export default client;
-  
